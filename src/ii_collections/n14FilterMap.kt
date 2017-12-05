aba576ed4d9c8ea8c019970f9f6c6f9be59ebf49
Tom Fuller <tfuller@thoughtworks.com>
2017-12-05 08:31:56 +0000

    Exercise 14 done

--- src/ii_collections/n14FilterMap.kt
+++ src/ii_collections/n14FilterMap.kt
@@ -10,12 +10,14 @@ fun example1(list: List<Int>) {
 
 fun Shop.getCitiesCustomersAreFrom(): Set<City> {
     // Return the set of cities the customers are from
-    todoCollectionTask()
+    val cities = mutableSetOf<City>()
+    customers.forEach { cities.add(it.city) }
+    return cities
 }
 
 fun Shop.getCustomersFrom(city: City): List<Customer> {
     // Return a list of the customers who live in the given city
-    todoCollectionTask()
+    return customers.filter { it.city == city }
 }
 
 
