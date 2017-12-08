2c33f493bc558990d2c112f31a20c61bff29a4e1
Tom Fuller <tfuller@thoughtworks.com>
2017-12-08 08:11:17 +0000

    Exercise 25 done

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,17 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+
+    private val dateStamp = year.toString() + month.toString().padStart(2, '0') + dayOfMonth.toString().padStart(2, '0')
+
+    override fun compareTo(other: MyDate) = when {
+        dateStamp < other.dateStamp -> -1
+        dateStamp > other.dateStamp -> 1
+        else -> 0
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
